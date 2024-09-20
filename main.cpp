73166b5287eb6469586630b4c2858d8a4b716941
shaots <shaots@mail.ru>
2024-09-20 15:45:51 +0300

    6 commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 int i = 0;
 
 int main() {
+    std::vector<int> vec;
+    std::cout << vec.end() - vec.begin() << std::endl;
     std::cout << "Hello world, my love" << std::endl;
     std::cout << "Let's go to Berlin" << std::endl;
     return 0;
